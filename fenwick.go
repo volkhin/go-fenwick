a4be27db0c56f6096755a1061af0e64e387bc39a
Artem Volkhin <artem@volkhin.com>
2014-07-29 15:58:34 +0400

    add comments

--- fenwick.go
+++ fenwick.go
@@ -1,19 +1,23 @@
+/* Fenwick tree (https://en.wikipedia.org/wiki/Fenwick_tree) */
 package fenwick
 
 import "errors"
 
 var IndexError = errors.New("Index is out of range")
 
+// Basic structure
 type Fenwick struct {
 	data []int
 }
 
+// New creates a Fenwick tree of fixed size
 func New(size uint) *Fenwick {
 	var f Fenwick
 	f.data = make([]int, size)
 	return &f
 }
 
+// Increment value at position by value
 func (f *Fenwick) Add(pos int, value int) error {
 	if pos < 0 || pos >= len(f.data) {
 		return IndexError
@@ -24,6 +28,7 @@ func (f *Fenwick) Add(pos int, value int) error {
 	return nil
 }
 
+// Return sum of value on segment [begin, end] (including both begin and end)
 func (f *Fenwick) Sum(begin, end int) int {
 	endSum := f.prefixSum(end)
 	beginSum := f.prefixSum(begin - 1)
